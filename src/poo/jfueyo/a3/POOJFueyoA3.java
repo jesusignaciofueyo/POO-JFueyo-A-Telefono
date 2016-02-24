1b9698520240ee0f4aa578bddfe2d046332e914e
jesus_ignacio_159 <jesus_ignacio_159@10.34.141.7>
2016-02-24 14:41:33 -0500

    first commit

--- src/poo/jfueyo/a3/POOJFueyoA3.java
+++ src/poo/jfueyo/a3/POOJFueyoA3.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package poo.jfueyo.c7;
+package poo.jfueyo.a3;
 
 /**
  *
  * @author jesus_ignacio_159
  */
-public class POOJFueyoC7 {
+public class POOJFueyoA3 {
 
     /**
      * @param args the command line arguments
--- src/poo/jfueyo/a3/Tecla.java
+++ src/poo/jfueyo/a3/Tecla.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package poo.jfueyo.c7;
+package poo.jfueyo.a3;
 
 /**
  *
