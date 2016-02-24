288680ef9cb497313049cef9300001804b37200f
jesus_ignacio_159 <jesus_ignacio_159@10.34.141.7>
2016-02-24 14:33:01 -0500

    first commit

---
+++ src/poo/jfueyo/c7/POOJFueyoC7.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.jfueyo.c7;
+
+/**
+ *
+ * @author jesus_ignacio_159
+ */
+public class POOJFueyoC7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Telefono starTac = new Telefono();
+        starTac.EA.presionar();
+        System.out.println("El starTac está " + starTac.EA.IO);
+        
+        starTac.setTeclado();
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(starTac.Teclado[i][j].getDigito());
+            }
+        }
+    }
+    
+}
